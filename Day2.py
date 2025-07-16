b3ede673d4f47554aa2164f57ec57c361d20b0e5
Ganesh <ganesh02003@gmail.com>
2025-07-16 21:59:11 +0530

    Python day learning

---
+++ Day2.py
@@ -0,0 +1,9 @@
+#Tip calculator
+print("Welcome to the tip calculator")
+total_bill = float(input("What was the total bill? Rs: "))
+tip = int(input("How much tip would you like to give? 10, 12 or 15? "))
+no_of_people = int(input("How many people to split the bill? "))
+
+total_amount = total_bill + (tip/100 * total_bill)
+split = round(total_amount/no_of_people, 2);
+print(f"Each person should pay: Rs {split}")
