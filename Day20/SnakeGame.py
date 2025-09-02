f806448c2a907f5cc685f3206ee7724ef3dba9bf
Ganesh <ganesh02003@gmail.com>
2025-09-02 07:57:30 +0530

    Moving snakes efficiently and turing left or right

--- Day20/SnakeGame.py
+++ Day20/SnakeGame.py
@@ -2,7 +2,7 @@ from turtle import Turtle, Screen
 import time
 
 screen = Screen()
-# screen.tracer(0)
+screen.tracer(0)
 
 screen.setup(width=600, height=600)
 screen.title("Snake Game")
@@ -18,20 +18,19 @@ for position in tut_positions:
     new_segment.goto(position)
     segments.append(new_segment)
 
-# is_game_on = True
-# while is_game_on:
-#     screen.update()
-#     time.sleep(0.5)
-#     for segment in segments:
-#         segment.forward(20)
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
 
+    # for seg_num in range(start=1, stop=0, step=-1)
+    for seg_num in range(len(segments)-1, 0, -1):
+        new_x = segments[seg_num-1].xcor()
+        new_y = segments[seg_num-1].ycor()
+        segments[seg_num].goto(new_x, new_y)
 
-#     # for seg_num in range(start=1, stop=0, step=-1)
-#     for seg_num in range(len(segments)-1, 0, -1):
-#         new_x = segments[seg_num-1].xcor()
-#         new_y = segments[seg_num-1].ycor()
-#         segments[seg_num].goto(new_x, new_y)
-
-#     segments[0].forward(20)
-
+    segments[0].forward(20)
+    segments[0].left(90)
+    
+    
 screen.exitonclick()
