61bf01456d0662a6e04db8d900dc2f5b54bf6e29
Ganesh <ganesh02003@gmail.com>
2025-09-02 07:54:46 +0530

    Screen setup and snake creation

---
+++ Day20/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+is_game_on = True
+# while is_game_on:
+#     screen.update()
+#     time.sleep(0.5)
+#     for segment in segments:
+#         segment.forward(20)
+
+
+#     # for seg_num in range(start=1, stop=0, step=-1)
+#     for seg_num in range(len(segments)-1, 0, -1):
+#         new_x = segments[seg_num-1].xcor()
+#         new_y = segments[seg_num-1].ycor()
+#         segments[seg_num].goto(new_x, new_y)
+
+#     segments[0].forward(20)
