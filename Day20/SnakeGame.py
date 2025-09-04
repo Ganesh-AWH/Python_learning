f61306e817cd834c700a1c668e530043b7dd77a7
Ganesh <ganesh02003@gmail.com>
2025-09-04 22:28:13 +0530

    Created food class and detecting that food

--- Day20/SnakeGame.py
+++ Day20/SnakeGame.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -11,16 +12,24 @@ screen.bgcolor("black")
 
 snake = Snake()
 
+screen.listen()
+food = Food()
+
+
+screen.onkey(key="Up", fun=snake.move_up)
+screen.onkey(key="Down", fun=snake.move_down)
+screen.onkey(key="Left", fun=snake.move_left)
+screen.onkey(key="Right", fun=snake.move_right)
+
+
 is_game_on = True
 while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
     
-    screen.listen()
-    screen.onkey(key="Up", fun=snake.move_up)
-    screen.onkey(key="Down", fun=snake.move_down)
-    screen.onkey(key="Left", fun=snake.move_left)
-    screen.onkey(key="Right", fun=snake.move_right)
+    #detecting collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
     
 screen.exitonclick()
