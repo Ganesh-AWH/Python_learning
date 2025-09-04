fc9d008cfc2ac43bc1dac2887cae73a7be2c14bb
Ganesh <ganesh02003@gmail.com>
2025-09-04 23:11:08 +0530

    Detect collison with wall

--- Day20/SnakeGame.py
+++ Day20/SnakeGame.py
@@ -33,4 +33,9 @@ while is_game_on:
         food.refresh()
         score.update_score()
     
+    #detecting collision with wall
+    if snake.head.xcor() > 280 or snake.head.ycor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280:
+        score.game_over()
+        is_game_on = False
+    
 screen.exitonclick()
