a8b8b0b3ebb83e9f79d81296076625cc5228d7ad
Ganesh <ganesh02003@gmail.com>
2025-09-07 09:22:11 +0530

    Creating paddle

--- Day21/PingPong.py
+++ Day21/PingPong.py
@@ -1,9 +1,35 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
+#screen setup
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("ping pong")
+screen.tracer(0)
 
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(380, 0)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+    
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(key="Up", fun=go_up)
+screen.onkey(key="Down", fun=go_down)
+
+is_game_on = True
+while is_game_on:
+    screen.update()
 
 screen.exitonclick()
+
