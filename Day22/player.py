66429b4cd02ff261317d3c9e1318fc77d04cb006
Ganesh <ganesh02003@gmail.com>
2025-09-08 12:55:26 +0530

    created cars that constantly moves along x-axis

--- Day22/player.py
+++ Day22/player.py
@@ -7,7 +7,7 @@ FINISH_LINE_Y = 280
 class Player(Turtle):
     def __init__(self):
         super().__init__()
-        self.color("black")
+        self.color("green")
         self.shape("turtle")
         self.penup()
         self.goto(STARTING_POSITION)
--- Day22/turtle_crossing.py
+++ Day22/turtle_crossing.py
@@ -1,15 +1,18 @@
 import time
 from turtle import Screen
 from player import Player
+from car_manager import CarManager
 # from car_manager import CarManager
 # from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor("black")
 screen.tracer(0)
 
 player = Player()
 game_is_on = True
+car = CarManager()
 
 screen.listen()
 screen.onkey(key="Up", fun=player.move)
@@ -17,6 +20,8 @@ screen.onkey(key="Up", fun=player.move)
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
+    car.create_cars()    
+    car.run()
+    
 
 screen.exitonclick()
