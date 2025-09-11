9c284f47aa0b2999add696b22132738a71b5740d
Ganesh <ganesh02003@gmail.com>
2025-09-11 23:11:38 +0530

    added extra features to the game

--- Day25/US States game/us_states_game.py
+++ Day25/US States game/us_states_game.py
@@ -15,17 +15,24 @@ turtle.shape(image)
 us_df = pd.read_csv(r"Day25\US States game\50_states.csv")
 all_states = us_df.state.to_list()
 
-total_count = len(us_df)
-guess_count = 0
+guessed_states = []
 
-
-while guess_count < 2:
+while len(guessed_states) < 50:
 
     #taking the state from user
-    answer = screen.textinput(title=f"{guess_count}/{total_count} correct", prompt="what's another state's name?").title()
-    #capitalizing the text
-    # capitalized_state = answer_state.capitalize()
-
+    answer = screen.textinput(title=f"{len(guessed_states)}/ 50 correct", prompt="what's another state's name?").title()
+    
+    if answer == "Exit":
+        missed_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missed_states.append(state)
+        
+        #taking the missing values list to new csv file  "states to learn.csv"
+        new_data = pd.DataFrame(missed_states)
+        new_data.to_csv(r"Day25\US States game\states to learn.csv")
+        break
+        
     if answer in all_states:
         #create the turtle class to writ in map
         tut = turtle.Turtle()
@@ -39,6 +46,6 @@ while guess_count < 2:
         
         tut.goto(x_cor, y_cor)
         tut.write(f"{answer}", font = ("Arial", 8, "normal"))
-        guess_count += 1
+        guessed_states.append(answer)
 
-screen.exitonclick()
+
