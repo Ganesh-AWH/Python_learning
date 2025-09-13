e78a56fb90b56e3d662ef108e2ef11450727b92e
Ganesh <ganesh02003@gmail.com>
2025-09-13 11:12:50 +0530

    phonetic code words exercise completed

---
+++ Day26/NATO-alphabet-start/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+df = pd.read_csv(r"Day26\NATO-alphabet-start\nato_phonetic_alphabet.csv")
+code = {row.letter: row.code for (index, row) in df.iterrows()}
+# print(code)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ")
+phonetic_code = [code[letter.upper()] for letter in word if letter in code]
+print(phonetic_code)
