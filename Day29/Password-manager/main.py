51a1afb05895005fec78e81598ab53d4f1cdad92
Ganesh <ganesh02003@gmail.com>
2025-09-23 18:57:38 +0530

    project

---
+++ Day29/Password-manager/main.py
@@ -0,0 +1,38 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+#canvas
+canvas = Canvas(width=200, height=200)
+lock_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_image)
+canvas.grid(row=0, column=1)
+
+#labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+#entries
+web_entry = Entry(width=45)
+web_entry.grid(row=1, column=1, columnspan=2, sticky='w') 
+email_entry = Entry(width=45)
+email_entry.grid(row=2, column=1, columnspan=2, sticky='w')
+password_entry = Entry(width=31)
+password_entry.grid(row=3, column=1, sticky='w')
+
+#buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2, sticky='w')
+add_button = Button(text="Add", width=45)
+add_button.grid(row=4, column=1, columnspan=2, sticky='w')
+window.mainloop()
