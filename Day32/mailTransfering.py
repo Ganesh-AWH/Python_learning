67456aeea9fe880063500bfd563e7e1b56cba6fa
Ganesh <ganesh02003@gmail.com>
2025-09-27 11:34:15 +0530

    learning how to send simple mail using smtp

---
+++ Day32/mailTransfering.py
@@ -0,0 +1,12 @@
+import smtplib
+
+my_mail = "anonymouswhitehack746@gmail.com"
+password = "bsdx mxcb rkvn hxhm"
+
+
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_mail, password=password)
+    connection.sendmail(from_addr=my_mail, to_addrs="ganesh02003@gmail.com", msg="Subject:Hello\n\nI am from body message")
+    connection.close()
