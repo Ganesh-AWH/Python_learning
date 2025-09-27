f3897b69dbe73ad93d20cdf6042c6489b41e5efc
Ganesh <ganesh02003@gmail.com>
2025-09-27 15:33:41 +0530

    updated

--- Day32/mailTransfering.py
+++ Day32/mailTransfering.py
@@ -9,4 +9,3 @@ with smtplib.SMTP("smtp.gmail.com") as connection:
     connection.starttls()
     connection.login(user=my_mail, password=password)
     connection.sendmail(from_addr=my_mail, to_addrs="ganesh02003@gmail.com", msg="Subject:Hello\n\nI am from body message")
-    connection.close()
